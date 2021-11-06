5791ff5b747676763c1ec222a9ed83108869b9e5
Sarah Romanhol Falconiere <sarahromanhol@gmail.com>
2021-11-06 13:38:30 -0300

    Formatação do date picker

--- hospedin/src/pages/HotelPage/HotelPage.js
+++ hospedin/src/pages/HotelPage/HotelPage.js
@@ -1,14 +1,36 @@
+import { useEffect, useState } from 'react'
+import { useHistory } from 'react-router'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import { getRooms } from '../../services/requests'
+import { goToPurchasePage } from '../../routes/coordinator'
 import { HotelPageDetails } from './HotelDetails/HotelPageDetails'
-import { HotelPageContainer, IconeUsers, BodyInfoRoom, Room, AmmenitiesTable, AmmenityContainer, DetailsTitle, DetailsBody, TableLables, BotaoMaisInformacoes, PrecoPorNoite, RoomPicture, MainInfo, HotelPageReservation, ReservationRoomContainer, DatePicker, ReservationDateContainer } from './styled'
 import arrowDown from '../../assets/imgs/Vectorarrow.png'
 import arrowUp from '../../assets/imgs/chevron-up.png'
 import calendar from '../../assets/imgs/calendar.png'
 import users from '../../assets/imgs/users.png'
 import check from '../../assets/imgs/fe-check-circle.png'
-import { getRooms } from '../../services/requests'
-import { useEffect, useState } from 'react'
-import { goToPurchasePage } from '../../routes/coordinator'
-import { useHistory } from 'react-router'
+import {
+    HotelPageContainer,
+    IconeUsers, 
+    BodyInfoRoom,
+    Room, 
+    AmmenitiesTable,
+    Ammenity,
+    DetailsTitle,
+    DetailsBody,
+    TableLables,
+    MoreInfoButton,
+    PrecoPorNoite,
+    RoomPicture,
+    MainInfo,
+    HotelPageReservation,
+    ReservationRoomContainer,
+    DatePickerContainer,
+    IndividualDatePicker,
+    StyledDatePicker,
+    ReservationDateContainer
+} from './styled'
 
 export const HotelPage = () => {
     const history = useHistory()
@@ -16,21 +38,30 @@ export const HotelPage = () => {
     const [showInfo, setShowInfo] = useState(false)
     const [selectedItemId, setSelectedItemId] = useState('')
     const [changeArrow, setChangeArrow] = useState(false)
+    const [selectedFirstDate, setSelectedFirstDate] = useState(null)
+    const [selectedSecondDate, setSelectedSecondDate] = useState(null)
 
-    useEffect(() => {
+    // useEffect(() => {
+    //     getRooms(setRooms)
+    // }, [])
+
+    const CallRooms = (date) => {
+        setSelectedSecondDate(date)
         getRooms(setRooms)
-    }, [])
+    }
 
+    //passando por params o nome e preço do quarto selecionado
     const bookRoom = (nome, preco) => {
         goToPurchasePage(history, nome, preco)
     }
 
+    //função que controla o botão de mais informações do quarto
     const maisInfo = (itemId) => {
         setShowInfo(prevState => !prevState)
         setSelectedItemId(itemId)
         setChangeArrow(prevState => !prevState)
     }
-    
+
 
     const renderRoomDetails = rooms.map((room, index) => {
         return (
@@ -45,34 +76,36 @@ export const HotelPage = () => {
                     </div>
                     <button onClick={() => bookRoom(room.name, room.amount)}>Reservar</button>
                 </MainInfo>
-                {showInfo && selectedItemId === index && (<BodyInfoRoom>
-                    <div>
-                        <DetailsTitle>Sobre o quarto</DetailsTitle>
-                        <DetailsBody>{room.description}</DetailsBody>
-                    </div>
-                    <div>
-                        <DetailsTitle>Acomodações</DetailsTitle>
-                        <DetailsBody>{room.accommodation}</DetailsBody>
-                    </div>
-                    <div>
-                        <DetailsTitle>Comodidades</DetailsTitle>
-                        <AmmenitiesTable>
-                            {room.amenities.map((ammenity) => {
-                                return (
-                                    <AmmenityContainer key={ammenity}>
-                                        <img src={check} />
-                                        <DetailsBody>{ammenity}</DetailsBody>
-                                    </AmmenityContainer>
-                                )
-                            })}
-                        </AmmenitiesTable>
-                    </div>
-                </BodyInfoRoom>)}
+                {showInfo && selectedItemId === index && (
+                    <BodyInfoRoom>
+                        <div>
+                            <DetailsTitle>Sobre o quarto</DetailsTitle>
+                            <DetailsBody>{room.description}</DetailsBody>
+                        </div>
+                        <div>
+                            <DetailsTitle>Acomodações</DetailsTitle>
+                            <DetailsBody>{room.accommodation}</DetailsBody>
+                        </div>
+                        <div>
+                            <DetailsTitle>Comodidades</DetailsTitle>
+                            <AmmenitiesTable>
+                                {room.amenities.map((ammenity) => {
+                                    return (
+                                        <Ammenity key={ammenity}>
+                                            <img src={check} />
+                                            <DetailsBody>{ammenity}</DetailsBody>
+                                        </Ammenity>
+                                    )
+                                })}
+                            </AmmenitiesTable>
+                        </div>
+                    </BodyInfoRoom>
+                )}
 
-                <BotaoMaisInformacoes onClick={() => maisInfo(index)}>
-                    <img src={changeArrow ? arrowUp : arrowDown}  />
+                <MoreInfoButton onClick={() => maisInfo(index)}>
+                    <img src={changeArrow ? arrowUp : arrowDown} />
                     <p>Mais informações</p>
-                </BotaoMaisInformacoes>
+                </MoreInfoButton>
             </Room>
         )
     })
@@ -83,13 +116,39 @@ export const HotelPage = () => {
             <HotelPageReservation>
                 <ReservationDateContainer>
                     <h4>Reserve a sua acomodação</h4>
-                    <DatePicker>
+                    {/* <DatePickerContainer>
                         <p>Dom, 17 Nov</p>
                         <img src={arrowDown} />
                         <p>Ter, 19 Nov</p>
                         <img src={arrowDown} />
                         <img src={calendar} />
-                    </DatePicker>
+                    </DatePickerContainer> */}
+                    <DatePickerContainer>
+                        <IndividualDatePicker>
+                            <StyledDatePicker
+                                selected={selectedFirstDate}
+                                onChange={date => setSelectedFirstDate(date)}
+                                minDate={new Date()}
+                                dateFormat='dd/MM/yyyy'
+                            />
+                            <img src={arrowDown} />
+                        </IndividualDatePicker>
+                        <IndividualDatePicker>
+                            <StyledDatePicker
+                                selected={selectedSecondDate}
+                                minDate={selectedFirstDate}
+                                onChange={date => CallRooms(date)}
+                                dateFormat='dd/MM/yyyy'
+                            />
+                            <img src={arrowDown} />
+                        </IndividualDatePicker>
+                        <img src={calendar} />
+                        {/* <p>Dom, 17 Nov</p>
+                        <img src={arrowDown} />
+                        <p>Ter, 19 Nov</p>
+                        <img src={arrowDown} />
+                        <img src={calendar} /> */}
+                    </DatePickerContainer>
                 </ReservationDateContainer>
                 <ReservationRoomContainer>
                     <TableLables>
--- hospedin/src/pages/HotelPage/styled.js
+++ hospedin/src/pages/HotelPage/styled.js
@@ -1,12 +1,13 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+import DatePicker from "react-datepicker";
 
 export const HotelPageContainer = styled.div`
+    margin-bottom: 100px;
+    padding-top: 70px;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding-top: 70px;
-    min-height: 100vh;
-    margin-bottom: 100px;
 `
 
 export const HotelPageReservation = styled.div`
@@ -16,42 +17,54 @@ export const HotelPageReservation = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    
 `
 
 export const ReservationDateContainer = styled.div`
     width: 930px;
     margin-top: 30px;
     h4{
+        margin: 0px;
         font-family: Poppins;
         font-size: 24px;
-        font-style: normal;
         font-weight: 700;
-        text-align: left;
         color: #29415E;
-        margin: 0px;
     }
 `
 
-export const DatePicker = styled.div`
+export const DatePickerContainer = styled.div`
+    background-color: #FFFFFF;
+    margin-top: 10px;
+    width: 434px;
+    height: 56px;
+    border: 1px solid #CFD9E7;
     display: flex;
     align-items: center;
     justify-content: space-around;
-    width: 434px;
-    height: 56px;
-    background-color: #FFFFFF;
     cursor: pointer;
-    margin-top: 10px;
-    border: 1px solid #CFD9E7;
     p{
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-family: Roboto;
         font-size: 16px;
+        color: #536580;
     }
 `
 
+export const IndividualDatePicker = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+export const StyledDatePicker = styled(DatePicker)`
+    border: none;
+    width: 150px;
+    height: 50px;
+    font-family: Roboto;
+    font-size: 16px;
+    color: #536580;
+`
+
 export const ReservationRoomContainer = styled.div`
-    width: 930px;
     margin-top: 30px;
+    width: 930px;
 `
 
 export const TableLables = styled.div`
@@ -70,25 +83,29 @@ export const PrecoPorNoite = styled.p`
     margin-right: 15px;
 `
 
+export const Room = styled.div`
+    margin-bottom: 20px;
+`   
+
 export const MainInfo = styled.div`
     background-color: #FFFFFF;
-    border: 1px solid #CFD9E7;
     height: 88px;
+    border: 1px solid #CFD9E7;
     display: grid;
     align-items: center;
     grid-template-columns: 1fr 2fr 1fr 1fr 2fr;
     button {
         background-color: #FBDE64;
-        color: #29415E;
-        font-family: Poppins;
-        font-size: 18px;
-        border-radius: 100px;
+        margin-right: 20px;
         width: 127px;
         height: 63px;
+        border-radius: 100px;
         border: none;
-        cursor: pointer;
         justify-self: end;
-        margin-right: 20px;
+        font-family: Poppins;
+        font-size: 18px;
+        color: #29415E;
+        cursor: pointer;
         &:hover{
             background-color: #d3b94c;
         }
@@ -97,15 +114,15 @@ export const MainInfo = styled.div`
         }
     }
     div{
+        margin-left: 20px;
         display: flex;
         align-items: center;
-        margin-left: 20px;
     }
     p{
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        color: #29415E;
+        font-family: Roboto;
         font-weight: 400;
         font-size: 18px;
+        color: #29415E;
     }
 `
 
@@ -114,69 +131,57 @@ export const RoomPicture = styled.img`
     height: 86px;
 `
 
-export const BotaoMaisInformacoes = styled.a`
+export const MoreInfoButton = styled.a`
+    background-color: #FFFFFF;
+    padding-left: 10px;
+    height: 40px;
+    border: 1px solid #CFD9E7;
+    box-shadow: 0px 1px 5px 1px #CFD9E7;
     display: flex;
     flex-direction: row;
     align-items: center;
-    background-color: #FFFFFF;
-    border: 1px solid #CFD9E7;
-    height: 40px;
-    box-shadow: 0px 1px 5px 1px #CFD9E7;
-    padding-left: 10px;
     cursor: pointer;
     p{
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        margin-left: 10px;
+        font-family: Roboto;
         font-weight: 400;
         font-size: 14px;
         color: #536580;
-        margin-left: 10px;
-    }
-    #arrowUp{
-        display: none;
     }
 `
 
-export const Room = styled.div`
-    margin-bottom: 20px;
-    #maisInfo{
-        display: none;
-    }
+export const BodyInfoRoom = styled.div`
+    background-color: #FFFFFF;
 `
 
 export const DetailsTitle = styled.p`
-    font-family: Poppins;
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 24px;
-    color: #29415E;
     margin-bottom: 0px;
     margin-top: 0px;
     padding-top: 10px;
+    font-family: Poppins;
+    font-size: 16px;
+    font-weight: 700;
+    color: #29415E;
 `
 
 export const DetailsBody = styled.p`
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: Roboto;
     font-size: 16px;
     font-weight: 400;
     color:  #536580;
 `
-
-export const AmmenityContainer = styled.div`
-    display: flex;
-    align-items: center;
-    img{
-        margin-right: 7px;
-    }
-`
-
 export const AmmenitiesTable = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     align-items: flex-start;
 `
 
-export const BodyInfoRoom = styled.div`
-    background-color: #FFFFFF;
+export const Ammenity = styled.div`
+    display: flex;
+    align-items: center;
+    img{
+        margin-right: 7px;
+    }
 `
 
 export const IconeUsers = styled.img`
